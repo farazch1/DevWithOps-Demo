958158e25d944adcd29d4f4b49ea2f7b3f1af17a
Mian Faraz.Dev <mianfaraz2002@gmail.com>
2024-06-12 23:50:01 +0500

    Upated

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+Must install following libraries before running the code
+
+pip install flask
+
+pip install email_validator
+
+pip install pywebview[cef]
+
+pip install flask_bcrypt
+
+pip install flask_wtf
+
+pip install flask_login
+
+pip install flask_sqlalchemy
--- test.py
+++
@@ -1,8 +0,0 @@
-from dWo import app
-from dWo.models import db, User, Course
-
-with app.app_context():
-    rel1 = Course.query.filter_by(course_name='Cloud Intro').first()
-    rel1.owner = User.query.filter_by(username='syed').first().id
-    db.session.add(rel1)
-    db.session.commit()
